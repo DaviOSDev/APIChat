0f59ef30c2fff328b76944ae4c131fb178019eb2
Davi Santos <davikpi51@gmail.com>
2024-04-14 14:34:40 -0300

    first commit

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+import os 
+
+# Install requirements
+os.system("pip install -r configs/requirements.txt")
+
+# Import modules
---
+++ src/main.py
@@ -0,0 +1,4 @@
+from os import system
+
+if __name__ == '__main__':
+    system("python3 src/__init__.py")
